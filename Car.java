dccdc8048c77a4bd325ff33d656c45bca88f04cd
Poojadula <poojabalasankar@gmail.com>
2026-02-03 01:22:48 +0530

    Added ArithmeticOperations.java

---
+++ Car.java
@@ -0,0 +1,30 @@
+class Car
+{
+public static void main(String[] args)
+{
+
+	String brand = "Tesla";
+	String fueltype = "Electric";
+	int mileage = 396;
+	float price = 85000.00f;
+	{
+		System.out.println("car name is " + brand);
+        System.out.println("car type is " + fueltype);
+		System.out.println("car type is " + mileage);
+		System.out.println("car $ is " + price);
+
+	
+{
+
+	}
+	
+	
+	
+}
+
+	
+	
+	
+	
+	
+}
--- Food.java
+++ Food.java
@@ -1,20 +1,22 @@
 class Food
 {
 public static void main(String[] args)
+
 {
-	string foodName = "Panneer Butter Masala";
-	isvegetarian = true;
-	calories = 400;
-	price = 250.50;
-	
-	System.out.println(foodName);
-	System.out.println(isvegetarian);
+	String Foodname = " Paneer Butter Masala  ";
+	boolean vegetarian = true;
+	int calories = 400;
+	float price = 250.50f;
+
+	System.out.println(Foodname);
+	System.out.println(vegetarian);
 	System.out.println(calories);
 	System.out.println(price);
 	
 	
 
 }
+
 
 
 
