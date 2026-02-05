41f93f0fba214993d927b29ebc4af1d37ae2d02e
Poojadula <poojabalasankar@gmail.com>
2026-02-06 00:27:21 +0530

    Added all files

---
+++ Languageselection.java
@@ -0,0 +1,82 @@
+class Languageselection
+{
+ public static void main(String[] args)	{
+	 int a = 4;
+	 switch (a)
+	 {
+		case 1:
+		{
+			System.out.println("Number 1 is Tamil");
+			break;
+		}
+			case 2:
+			{
+				System.out.println("Number 2 is English");
+				break;
+		    }
+		 case 3:
+		 {
+			 System.out.println("Number 3 is Hindi");
+			 break;
+		 }
+		 case 4:
+		 {
+			System.out.println("Number 4 is Malayalam");
+			break;
+			 
+			 
+		 }
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+	 }
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+ }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ Trafficsignal.java
@@ -0,0 +1,39 @@
+class Trafficsignal
+{
+  public static void main(String[] args)
+  {
+    System.out.print("Enter the signal color (R,Y,G): ");
+	Switch(signal){
+	
+		case'R':
+		System.out.println("Stop");
+		break;
+		
+		case'Y':
+		System.out.println("Ready");
+		break;
+		
+		case'G':
+		System.out.println("Go");
+	    break;
+	}	
+	}	
+	
+	}
+	
+	
+	
+	
+	
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+  }
+}
