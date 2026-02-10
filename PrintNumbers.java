bb49ccd211eac78aedadba62602aa3b74d9862f8
Poojadula <poojabalasankar@gmail.com>
2026-02-10 18:12:54 +0530

    Added PrintNumbers for loop program

---
+++ PrintNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class PrintNumbers
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter N value");
+		int n = sc.nextInt();
+		
+		for(int i = 1; i <= n; i++)
+		{
+			System.out.println(i);
+		}
+		   sc.close();
+	}
+	
+	
+}
