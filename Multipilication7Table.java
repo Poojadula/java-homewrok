c3f5d9782bf686feb59ad244552ace74b0d0f328
Poojadula <poojabalasankar@gmail.com>
2026-02-11 23:21:55 +0530

    Added Mulitipilication7Table program

---
+++ Multipilication7Table.java
@@ -0,0 +1,13 @@
+class Multipilication7Table
+{
+	public static void main(String[] args)
+	{
+		int number = 7;
+		
+		for(int i = 7; i <= 10; i++)
+		{
+			System.out.println(number + " x " + i + " = " + (number * i ));
+		}	
+	}
+		
+}
