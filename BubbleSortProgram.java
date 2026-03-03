4593b4dd920326958eab2a876b67efbb29a81c31
Poojadula <poojabalasankar@gmail.com>
2026-03-03 19:10:33 +0530

    Added BubbleSortProgram

---
+++ BubbleSortProgram.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class BubbleSortProgram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Read number of elements
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        // Read array elements
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int passes = 0;
+
+        // Bubble Sort
+        for (int i = 0; i < n - 1; i++) {
+            passes++;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        // Print sorted array
+        System.out.println("Sorted Array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        // Print number of passes
+        System.out.println("\nNumber of Passes: " + passes);
+
+        sc.close();
+    }
+}
